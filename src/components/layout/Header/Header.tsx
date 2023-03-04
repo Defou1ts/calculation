6b1377bb8798486efa323a3e04c6f168d7ecc355
Defou1ts <pasnkevikn@gmail.com>
2023-03-04 11:43:46 +0300

    create header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { HOME_CC_ROUTE, HOME_FC_ROUTE, SETTINGS_CC_ROUTE, SETTINGS_FC_ROUTE } from '@constants';
+import { HeaderWrapper, Navigation, StyledNavLink } from './styled';
 
 export const Header = (): JSX.Element => {
-	return <p>Header</p>;
+	return (
+		<HeaderWrapper>
+			<Navigation>
+				<StyledNavLink to={HOME_FC_ROUTE}>Home FC</StyledNavLink>
+				<StyledNavLink to={HOME_CC_ROUTE}>Home CC</StyledNavLink>
+				<span></span>
+				<StyledNavLink to={SETTINGS_FC_ROUTE}>Settings FC</StyledNavLink>
+				<StyledNavLink to={SETTINGS_CC_ROUTE}>Settings CC</StyledNavLink>
+			</Navigation>
+		</HeaderWrapper>
+	);
 };

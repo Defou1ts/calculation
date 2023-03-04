a08e358ed14cf534c7fd2871f72f0d24b151a021
Defou1ts <pasnkevikn@gmail.com>
2023-03-04 17:41:55 +0300

    fix layout styled components

--- src/components/layout/styled.ts
+++ src/components/layout/styled.ts
@@ -5,8 +5,11 @@ export const LayoutWrapper = styled.div`
 	grid-template-rows: 107px auto;
 	grid-template-columns: 1fr;
 	background-color: var(--background);
+	gap: 17px;
 
 	padding: 150px;
 
 	min-height: 100vh;
 `;
+
+

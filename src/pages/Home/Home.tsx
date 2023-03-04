4054f472095a4d438ca0f7b9a232c671cac3c2d1
Defou1ts <pasnkevikn@gmail.com>
2023-03-04 17:42:06 +0300

    created basic home pages

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Main } from './styled';
+
+export const Home = (): JSX.Element => {
+	return <Main></Main>;
+};
---
+++ src/pages/Home/styled.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+	display: grid;
+	grid-template-columns: minmax(705px, 1fr) auto;
+`;

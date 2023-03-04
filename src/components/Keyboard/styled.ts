343f408173993284fd88eb4de4cd1ec7f2d55717
Defou1ts <pasnkevikn@gmail.com>
2023-03-04 10:42:27 +0300

    fixed layout

--- src/components/Keyboard/styled.ts
+++ src/components/Keyboard/styled.ts
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const KeyboardWrapper = styled.div`
 	display: grid;
-	grid-template-rows: repeat(5, 1fr);
-	grid-template-columns: repeat(4, 1fr);
+	background: var(--content);
+	grid-template-rows: repeat(5, 70px);
+	grid-template-columns: repeat(4, 70px);
 	gap: 21px 112px;
 `;

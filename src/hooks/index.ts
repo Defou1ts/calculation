8d818fff1f3963728c16befb4446601cc7632ebb
Defou1ts <pasnkevikn@gmail.com>
2023-03-21 18:41:16 +0300

    fix: rafactoring

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,2 @@
 export * from './useCalculator';
-export * from './useCalculatorContext';
 export * from './useTheme';
--- src/hooks/useCalculatorContext.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-
-import { CalculatorContext } from '@interfaces';
-import type { ICalculatorContext } from '@interfaces';
-
-export const useCalculatorContext = (): ICalculatorContext => useContext(CalculatorContext);
